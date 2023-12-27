09866a89c30b537c9b28486da803316effd9dd67
Opmrph <121597474+Opmrph@users.noreply.github.com>
2023-12-27 20:41:48 +1100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+class config(object):
+    """Simple object to store our config"""
+    # after the edits I made and the way I ran it, I'm not sure if this does anything any more.
+    profile = 'hybri'
+    base_dir = '.'
